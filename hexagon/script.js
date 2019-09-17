9a7772fecd1cbafe338193258e7c048d6b9fe85b
Patrik Spathon <patrik@spathon.com>
2019-09-17 19:31:26 +0200

    Fix x, y and draw one hex at the time

--- hexagon/script.js
+++ hexagon/script.js
@@ -21,6 +21,13 @@ const HEX_SPACING = HEX_WIDTH * 3/4
 const ITEMS_WIDTH = WIDTH / HEX_SPACING
 const ITEMS_HEIGHT = HEIGHT / HEX_HEIGHT
 
+function oddqToCube(hex) {
+  const x = hex.x
+  const z = hex.y - (hex.x - (hex.x & 1)) / 2
+  const y = -x-z
+  return { x, y, z }
+}
+
 function Point(x, y) {
   this.x = x
   this.y = y
@@ -55,27 +62,36 @@ function Hex(x, y) {
 
 
 
-for (let i = 0; i < ITEMS_WIDTH; i++) {
-  for (let j = 0; j < ITEMS_HEIGHT; j++) {
-    const hex = new Hex(i, j)
+for (let iY = 0; iY < ITEMS_HEIGHT; iY++) {
+  for (let iX = 0; iX < ITEMS_WIDTH; iX++) {
+    const hex = new Hex(iX, iY)
     grid.push(hex)
-
-    ctx.beginPath()
-    ctx.moveTo(hex.points[0].x, hex.points[0].y)
-
-    for (let k = 1; k < hex.points.length; k++) {
-      ctx.lineTo(hex.points[k].x, hex.points[k].y)
-    }
-
-    ctx.closePath()
-    ctx.stroke()
-    // const hexGradient = ctx.createRadialGradient(hex.center.x, hex.center.y, 1, hex.center.x, hex.center.y, SIZE*2)
-    // hexGradient.addColorStop(0, '#000944')
-    // hexGradient.addColorStop(1, '#000020')
-    // ctx.fillStyle = hexGradient
-    ctx.fill()
   }
 }
 
+let g = 0
+function draw() {
+  const hex = grid[g]
+  g += 1
+  const cube = oddqToCube(hex)
+  console.log(cube, 'x:', hex.x, 'y', hex.y)
+  ctx.beginPath()
+  ctx.moveTo(hex.points[0].x, hex.points[0].y)
+
+  for (let k = 1; k < hex.points.length; k++) {
+    ctx.lineTo(hex.points[k].x, hex.points[k].y)
+  }
+
+  ctx.closePath()
+  ctx.stroke()
+  // const hexGradient = ctx.createRadialGradient(hex.center.x, hex.center.y, 1, hex.center.x, hex.center.y, SIZE*2)
+  // hexGradient.addColorStop(0, '#000944')
+  // hexGradient.addColorStop(1, '#000020')
+  // ctx.fillStyle = hexGradient
+  ctx.fill()
+  if(g < grid.length - 1) setTimeout(draw, 1)
+}
+  
+draw()
 
 
