bb6f26c3d5bb92cd2fa485a2c515254056767348
Patrik Spathon <patrik@spathon.com>
2019-09-18 17:24:56 +0200

    Draw player1 calc and animation WIP

--- hexagon/script.js
+++ hexagon/script.js
@@ -1,17 +1,27 @@
+import {
+  Hexa, Point, Hex
+} from './hex.js'
+
 
 const canvas = document.getElementById('board')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const evtCanvas = document.getElementById('interactive')
+const countDownEl = document.getElementById('countDown')
+canvas.width = evtCanvas.width = window.innerWidth
+canvas.height = evtCanvas.height = window.innerHeight
+
+const evtCtx = evtCanvas.getContext('2d')
+evtCtx.fillStyle = '#fff'
+evtCtx.strokeStyle = 'rgba(255, 255, 255, 1)'
+evtCtx.lineWidth = 2
 
 const ctx = canvas.getContext('2d')
 ctx.fillStyle = 'rgba(0, 0, 0, .5)'
 ctx.strokeStyle = '#007bd2'
-// ctx.fillStyle = '#007bd2'
-// ctx.strokeStyle = 'rgba(0, 0, 0, .5)'
 ctx.lineWidth = 1
 
 const SIZE = 15
-const TOTAL_SIZE = SIZE + 3
+const SPACING = 4
+const TOTAL_SIZE = SIZE + SPACING
 const grid = []
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
@@ -21,77 +31,160 @@ const HEX_SPACING = HEX_WIDTH * 3/4
 const ITEMS_WIDTH = WIDTH / HEX_SPACING
 const ITEMS_HEIGHT = HEIGHT / HEX_HEIGHT
 
-function oddqToCube(hex) {
-  const x = hex.x
-  const z = hex.y - (hex.x - (hex.x & 1)) / 2
-  const y = -x-z
-  return { x, y, z }
+const hexa = new Hexa({ size: SIZE, spacing: SPACING })
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function inRange(x, min, max) {
+  return ((x-min)*(x-max) <= 0)
 }
 
-function Point(x, y) {
-  this.x = x
-  this.y = y
-}
-
-function Hex(x, y) {
-  this.x = x
-  this.y = y
-  this.points = []
-
-  var offSetX = (TOTAL_SIZE / 2 * x) * -1
-  var offSetY = 0
-  if (x % 2 == 1) offSetY = Math.sqrt(3) / 2 * TOTAL_SIZE
-
-  this.center = new Point(
-    x * TOTAL_SIZE * 2 + offSetX,
-    y * Math.sqrt(3) / 2 * TOTAL_SIZE * 2 + offSetY
-  )
-
-  for (let i = 0; i < 6; i++) {
-    var degree = 60 * i
-    var radian = Math.PI / 180 * degree
-
-    var point = new Point(
-      this.center.x + SIZE * Math.cos(radian),
-      this.center.y + SIZE * Math.sin(radian)
-    )
-
-    this.points.push(point)
-  }
-}
-
-
-
-for (let iY = 0; iY < ITEMS_HEIGHT; iY++) {
-  for (let iX = 0; iX < ITEMS_WIDTH; iX++) {
-    const hex = new Hex(iX, iY)
-    grid.push(hex)
-  }
-}
-
-let g = 0
-function draw() {
-  const hex = grid[g]
-  g += 1
-  const cube = oddqToCube(hex)
-  console.log(cube, 'x:', hex.x, 'y', hex.y)
+function draw(cube, color) {
+  const center = hexa.flatHexToPixel(cube)
+  const corners = hexa.getCorners(center)
   ctx.beginPath()
-  ctx.moveTo(hex.points[0].x, hex.points[0].y)
+  ctx.moveTo(corners[0].x, corners[0].y)
 
-  for (let k = 1; k < hex.points.length; k++) {
-    ctx.lineTo(hex.points[k].x, hex.points[k].y)
+  for (let i = 1; i < corners.length; i++) {
+    ctx.lineTo(corners[i].x, corners[i].y)
   }
 
   ctx.closePath()
   ctx.stroke()
-  // const hexGradient = ctx.createRadialGradient(hex.center.x, hex.center.y, 1, hex.center.x, hex.center.y, SIZE*2)
-  // hexGradient.addColorStop(0, '#000944')
-  // hexGradient.addColorStop(1, '#000020')
-  // ctx.fillStyle = hexGradient
+  if (color) ctx.fillStyle = color
   ctx.fill()
-  if(g < grid.length - 1) setTimeout(draw, 1)
 }
-  
-draw()
 
 
+for (let iY = 0; iY < ITEMS_HEIGHT; iY++) {
+  for (let iX = 0; iX < ITEMS_WIDTH; iX++) {
+    const cube = hexa.oddqToCube(new Point(iX, iY))
+    grid.push(cube)
+    draw(cube)
+  }
+}
+
+
+const player = {
+  initPos: {}
+}
+
+function getDirection(value) {
+  if (value > 0) return -1
+  if (value < 0) return 1
+  return 0
+}
+
+
+function startGame() {
+  const center = hexa.flatHexToPixel(player.initPos)
+  const corners = hexa.getCorners(center, TOTAL_SIZE)
+  const randomNum = randomBetween(0, 5)
+  const corner = corners[randomNum]
+  const nextCorner = randomNum === 5 ? corners[0] : corners[randomNum + 1]
+  player.from = corner
+  player.to = nextCorner
+  player.amount = 0
+  player.directionX = getDirection(corner.x - nextCorner.x)
+  player.directionY = getDirection(corner.y - nextCorner.y)
+
+  drawCircle(corner)
+  evtCtx.moveTo(corner.x, corner.y)
+  animate()
+}
+
+function animate() {
+  player.amount += 0.1
+  const x = player.from.x + (player.to.x - player.from.x) * player.amount
+  const y = player.from.y + (player.to.y - player.from.y) * player.amount
+  console.log(x, y, player.to)
+  evtCtx.lineTo(x, y)
+  evtCtx.strokeStyle = 'rgba(255, 255, 255, 1)'
+  // evtCtx.lineWidth = 2
+  // evtCtx.shadowBlur = 3
+  evtCtx.shadowColor = 'rgba(255, 255, 255, .3)'
+  evtCtx.stroke()
+
+  // if (x < 0) {
+  //   console.log('WTF', x, y, player.to)
+  //   return ''
+  // }
+  if (inRange(x, player.to.x - 0.1, player.to.x + 0.1)) {
+    console.log('Init')
+    const hex = hexa.pixelToFlatHex(new Point(x + player.directionX, y + player.directionY))
+    const center = hexa.flatHexToPixel(hex)
+    const corners = hexa.getCorners(center, TOTAL_SIZE)
+    const endCorner = corners.findIndex(c => inRange(c.x, x - 1, x + 1) && inRange(c.y, y - 1, y + 1))
+    console.log('DONE', x, y, 'NEW', hex, 'CORNERS', corners, 'END', endCorner)
+    player.from = corners[endCorner]
+    if (!player.from) {
+      console.log('NOPE', corners, endCorner)
+    }
+    const randomNext = randomBetween(0, 1)
+    if (randomNext) {
+      player.to = endCorner === 5 ? corners[0] : corners[endCorner + 1]
+    } else {
+      player.to = endCorner === 0 ? corners[5] : corners[endCorner - 1]
+    }
+    player.amount = 0
+    player.directionX = getDirection(player.from.x - player.to.x)
+    player.directionY = getDirection(player.from.y - player.to.y)
+    draw(hex, '#911')
+    // drawCircle(corners[endCorner])
+    animate()
+  } else {
+    setTimeout(() => animate(), 10)
+  }
+}
+
+
+
+function drawCircle(corner) {
+  const radius = 5
+  evtCtx.arc(corner.x, corner.y, radius, 0, 2 * Math.PI)
+  evtCtx.strokeStyle = 'rgba(0, 0, 0, 1)'
+  evtCtx.lineWidth = 1
+  evtCtx.stroke()
+  evtCtx.fill()
+}
+
+function drawLine(from, to) {
+  evtCtx.beginPath()
+  evtCtx.moveTo(from.x, from.y)
+  evtCtx.lineTo(to.x, to.y)
+  evtCtx.strokeStyle = 'rgba(255, 255, 255, 1)'
+  evtCtx.lineWidth = 2
+  evtCtx.shadowBlur = 3
+  evtCtx.shadowColor = '#fff'
+  evtCtx.stroke()
+}
+
+
+function initDelay(delay) {
+  if (delay <= 0) {
+    countDownEl.textContent = ''
+    return startGame()
+  }
+  countDownEl.textContent = delay
+  setTimeout(() => initDelay(--delay), 700)
+}
+
+function start() {
+  // const startX = randomBetween(40, WIDTH - 40)
+  // const startY = randomBetween(40, HEIGHT - 40)
+  // const hex = hexa.pixelToFlatHex(new Point(startX, startY))
+  // player.initPos = hex
+  // draw(hex, '#119')
+
+  initDelay(3)
+}
+
+evtCanvas.addEventListener('click', (evt) => {
+  const offsetX = evt.pageX
+  const offsetY = evt.pageY
+  const hex = hexa.pixelToFlatHex(new Point(offsetX, offsetY))
+  player.initPos = hex
+  draw(hex, '#191')
+  start()
+})
